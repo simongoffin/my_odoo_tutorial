ee4e62b355da6abef1facebc282e4bf475ec8afc
Goffin Simon <simon.goffin@outlook.com>
2014-07-12 22:56:25 +0200

    Storing data in OpenERP with biography.

--- my-modules/academy/models/academy.py
+++ my-modules/academy/models/academy.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from openerp.models import Model
-from openerp.fields import Char
+from openerp.fields import Char, Html
 
 class TeachingAssistants(Model):
     _name = "academy.tas"
 
     name = Char()
+    biography = Html()
