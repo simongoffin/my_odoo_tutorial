d5bf3ab8cedde7e6be5c2f111619747edd8947b8
Goffin Simon <simon.goffin@outlook.com>
2014-07-12 20:31:11 +0200

    Hello world!

--- my-modules/academy/controllers/academy.py
+++ my-modules/academy/controllers/academy.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
+
 from openerp import http
+from openerp.addons.web.controllers import main
 
-
-class academy(http.Controller):
-    @http.route('/academy/academy/', auth='public', website=True)
-    def index(self, **kw):
-        return "Hello, world!"
-
-
-
-class academy(http.Controller):
+class academy(main.Home):
     @http.route('/', auth='none')
     def index(self):
         return """<!doctype html>
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-        <body class="container">
-           Hello, world!
-        </body>
-    """
+<link href="/web/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<body class="container">
+    Hello, world!
+</body>
+"""
 
-
-
-    @http.route('/academy/academy/<model("academy.academy"):academy>', type='http', auth='public', website=True)
-    def academy(self, academy, **kw):
-        return "Hello, %r!" % academy
 
