a4421e7030c83cc20d8c2d39faa523987c9e5e9e
Goffin Simon <simon.goffin@outlook.com>
2014-07-12 21:59:02 +0200

    Storing data in OpenERP without TA in the database.

--- my-modules/academy/controllers/academy.py
+++ my-modules/academy/controllers/academy.py
@@ -3,19 +3,12 @@
 from openerp import http
 from openerp.addons.web.controllers import main
 
-teaching_assistants = [
-    {'name': "Diana Padilla"},
-    {'name': "Jody Carroll"},
-    {'name': "Lester Vaughn"},
-    {'name': "Paul Jimenez"},
-    {'name': "Tanya Harris"},
-]
-
 class academy(main.Home):
     @http.route('/', auth='public', website=True)
     def index(self):
+        tas = http.request.env['academy.tas'].search([])
         return http.request.render('academy.index', {
-            'tas': teaching_assistants,
+            'tas': tas,
         })
 
     @http.route('/tas/<int:id>/', auth='public', website=True)
--- my-modules/academy/models/academy.py
+++ my-modules/academy/models/academy.py
@@ -2,8 +2,7 @@
 from openerp.models import Model
 from openerp.fields import Char
 
-
-class academy(Model):
-    _name = "academy.academy"
+class TeachingAssistants(Model):
+    _name = "academy.tas"
 
     name = Char()
