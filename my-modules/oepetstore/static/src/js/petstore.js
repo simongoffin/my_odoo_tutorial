5edeccfe184baab4ded4259a4cac0bcb003d5ab4
Goffin Simon <simon.goffin@outlook.com>
2014-07-14 09:41:45 +0200

    Heritage.

--- my-modules/oepetstore/static/src/js/petstore.js
+++ my-modules/oepetstore/static/src/js/petstore.js
@@ -14,6 +14,17 @@ openerp.oepetstore = function(instance) {
             console.log("hello", this.name);
         },
     });
+    
+    instance.oepetstore.MySpanishClass = instance.oepetstore.MyClass.extend({
+        say_hello: function() {
+            this._super();
+            console.log("translation in Spanish: hola", this.name);
+        },
+    });
+
+    var my_object = new instance.oepetstore.MySpanishClass("Nicolas");
+    my_object.say_hello();
+    // print "hello Nicolas \n translation in Spanish: hola Nicolas" in the console
 
     var my_object = new instance.oepetstore.MyClass("Simon");
     my_object.say_hello();
