317d2383ba92724a7bd283bc0beeb66756d52003
Goffin Simon <simon.goffin@outlook.com>
2014-07-14 12:28:05 +0200

    Usage of QWeb in Widgets.

--- my-modules/oepetstore/static/src/js/petstore.js
+++ my-modules/oepetstore/static/src/js/petstore.js
@@ -5,19 +5,23 @@ openerp.oepetstore = function(instance) {
     var QWeb = instance.web.qweb;
 
     instance.oepetstore = {};
-    
-    instance.oepetstore.HomePage = instance.web.Widget.extend({
-        template: "HomePageTemplate",
-        init: function(parent) {
-            this._super(parent);
-            this.names=["Simon","Moi","Ok"];
 
-        },
+    instance.oepetstore.HomePage = instance.web.Widget.extend({
         start: function() {
+            var products = new instance.oepetstore.ProductsWidget(this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
+            products.appendTo(this.$el);
         },
     });
-    
+
+    instance.oepetstore.ProductsWidget = instance.web.Widget.extend({
+        template: "ProductsWidget",
+        init: function(parent, products, color) {
+            this._super(parent);
+            this.products = products;
+            this.color = color;
+        },
+    });
+
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
-    
 }
 
