6c4c657addd94570bd893da2ced88c5abc985c38
Goffin Simon <simon.goffin@outlook.com>
2014-07-15 15:39:12 +0200

    Create a Color Field.

--- my-modules/oepetstore/static/src/js/petstore.js
+++ my-modules/oepetstore/static/src/js/petstore.js
@@ -27,36 +27,83 @@ openerp.oepetstore = function(instance) {
             var self = this;
             new instance.web.Model("message_of_the_day").query(["message"]).first().then(function(result) {
                 self.$(".oe_mywidget_message_of_the_day").text(result.message);
+                self.$(".oe_mywidget_message_of_the_day").css("color", result.color);
+                console.log("Coucou " + result.color);
+                self.$(".oe_mywidget_message_of_the_day").click(function() {
+                    self.message_clicked(result);
+                });
+            });
+        },
+        message_clicked: function(message) {
+            this.do_action({
+                type: 'ir.actions.act_window',
+                res_model: "message_of_the_day",
+                res_id: message.id,
+                views: [[false, 'form']],
+                target: 'current',
+                context: {},
             });
         },
     });
 
     instance.oepetstore.PetToysList = instance.web.Widget.extend({
-    template: "PetToysList",
-    start: function() {
-        var self = this;
-        new instance.web.Model("product.product").query(["name", "image"])
-            .filter([["categ_id.name", "=", "Pet Toys"]]).limit(5).all().then(function(result) {
-            _.each(result, function(item) {
-                var $item = $(QWeb.render("PetToy", {item: item}));
-                self.$el.append($item);
-                $item.click(function() {
-                    self.item_clicked(item);
+        template: "PetToysList",
+        start: function() {
+            var self = this;
+            new instance.web.Model("product.product").query(["name", "image"])
+                .filter([["categ_id.name", "=", "Pet Toys"]]).limit(5).all().then(function(result) {
+                _.each(result, function(item) {
+                    var $item = $(QWeb.render("PetToy", {item: item}));
+                    self.$el.append($item);
+                    $item.click(function() {
+                        self.item_clicked(item);
+                    });
                 });
             });
-        });
-    },
-    item_clicked: function(item) {
-        this.do_action({
-            type: 'ir.actions.act_window',
-            res_model: "product.product",
-            res_id: item.id,
-            views: [[false, 'form']],
-            target: 'current',
-            context: {},
-        });
-    },
-});
+        },
+        item_clicked: function(item) {
+            this.do_action({
+                type: 'ir.actions.act_window',
+                res_model: "product.product",
+                res_id: item.id,
+                views: [[false, 'form']],
+                target: 'current',
+                context: {},
+            });
+        },
+    });
+    
+    instance.oepetstore.FieldColor = instance.web.form.AbstractField.extend({
+        init: function() {
+            this._super.apply(this, arguments);
+            this.set("value", "");
+        },
+        start: function() {
+            this.on("change:effective_readonly", this, function() {
+                this.display_field();
+                this.render_value();
+            });
+            this.display_field();
+            return this._super();
+        },
+        display_field: function() {
+            var self = this;
+            this.$el.html(QWeb.render("FieldColor", {widget: this}));
+            if (! this.get("effective_readonly")) {
+                this.$("input").change(function() {
+                    self.internal_set_value(self.$("input").val());
+                });
+            }
+        },
+        render_value: function() {
+            if (this.get("effective_readonly")) {
+                this.$(".oe_field_color_content").css("background-color", this.get("value") || "#FFFFFF");
+            } else {
+                this.$("input").val(this.get("value") || "#FFFFFF");
+            }
+        },
+    });
 
+    instance.web.form.widgets.add('color', 'instance.oepetstore.FieldColor');
 }
 
