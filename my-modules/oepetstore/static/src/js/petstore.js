877830e7047e27b2d67fd61ac13fd85f996b1bd4
Goffin Simon <simon.goffin@outlook.com>
2014-07-15 11:38:33 +0200

    Pet Toys List.

--- my-modules/oepetstore/static/src/js/petstore.js
+++ my-modules/oepetstore/static/src/js/petstore.js
@@ -9,8 +9,10 @@ openerp.oepetstore = function(instance) {
     instance.oepetstore.HomePage = instance.web.Widget.extend({
         template: "HomePage",
         start: function() {
+            var pettoys = new instance.oepetstore.PetToysList(this);
+            pettoys.appendTo(this.$(".oe_petstore_homepage_left"));
             var motd = new instance.oepetstore.MessageOfTheDay(this);
-            motd.appendTo(this.$el);
+            motd.appendTo(this.$(".oe_petstore_homepage_right"));
         },
     });
 
@@ -25,6 +27,20 @@ openerp.oepetstore = function(instance) {
             var self = this;
             new instance.web.Model("message_of_the_day").query(["message"]).first().then(function(result) {
                 self.$(".oe_mywidget_message_of_the_day").text(result.message);
+            });
+        },
+    });
+
+    instance.oepetstore.PetToysList = instance.web.Widget.extend({
+        template: "PetToysList",
+        start: function() {
+            var self = this;
+            new instance.web.Model("product.product").query(["name", "image"])
+                .filter([["categ_id.name", "=", "Pet Toys"]]).limit(5).all().then(function(result) {
+                _.each(result, function(item) {
+                    var $item = $(QWeb.render("PetToy", {item: item}));
+                    self.$el.append($item);
+                });
             });
         },
     });
